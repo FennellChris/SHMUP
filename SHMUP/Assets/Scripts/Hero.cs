d9cc72be9fa71e2880c5df42c857769ebe8c52de
FennellChris <cjf169@scarletmail.rutgers.edu>
2023-02-21 14:24:21 -0500

    Scene Management
    Made a restart game scene upon hero death

--- SHMUP/Assets/Scripts/Hero.cs
+++ SHMUP/Assets/Scripts/Hero.cs
@@ -11,8 +11,11 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
     
-    [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")] [Range(0,4)] [SerializeField]
+    private float _shieldLevel = 1;
+    //public float shieldLevel = 1;
+    [Tooltip( "This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTiggerGo = null;
 
     void Awake () {
         if (S == null){
@@ -35,5 +38,31 @@ public class Hero : MonoBehaviour
 
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*rollMult,0);
         
+    }
+    void OnTriggerEnter(Collider other ) {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        //Debug.Log("Shield trigger hit by: " +go.gameObject.name);
+
+        if ( go == lastTiggerGo ) return;
+        lastTiggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if ( enemy != null ) {
+            shieldLevel --;
+            Destroy(go);
+        } else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " +go.name);
+        }
+    }
+    public float shieldLevel  {
+        get { return ( _shieldLevel ); }
+        private set {
+            _shieldLevel = Mathf.Min( value, 4 );
+            if (value < 0 ){
+                Destroy(this.gameObject);
+                Main.HERO_DIED();
+            }
+        }
     }
 }
--- SHMUP/Assets/Scripts/Main.cs
+++ SHMUP/Assets/Scripts/Main.cs
@@ -11,6 +11,7 @@ public class Main : MonoBehaviour
     public GameObject[] prefabEnemies;
     public float enemySpawnPerSecond = 0.5f;
     public float enemyInsetDefualt = 1.5f;
+    public float gameRestartDelay = 2; 
 
     private BoundsCheck bndCheck;
 
@@ -38,5 +39,14 @@ public class Main : MonoBehaviour
         go.transform.position = pos;
 
         Invoke ( nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+    void DelayedRestart() {
+        Invoke( nameof(Restart), gameRestartDelay);
+    }
+    void Restart() {
+        SceneManager.LoadScene("__Scene_0");
+    }
+    static public void HERO_DIED() {
+        S.DelayedRestart();
     }
 }
